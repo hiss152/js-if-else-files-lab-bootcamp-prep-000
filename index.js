b5e8887f3b69c61fad3c642cb4ea3e08a51aae0c
Daniel Rotchford <jimrockford45@gmail.com>
2017-09-14 19:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var name = "Susan"
+var height = "70"
+
+var name = "Joe"
 var height = "74"
 
 // Don't worry about this
@@ -9,4 +12,7 @@ if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
+} else if (name === "Joe") 
+{console.log("Expected the name to be Susan")
 }
+
